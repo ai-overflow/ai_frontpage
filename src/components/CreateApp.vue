07b8ee6ae4bfc55a70f2857bb6badcbda3eb5fdc
Sebastian <19Sebastian95@gmx.de>
2021-04-18 03:03:08 +0200

    update layout: added download info

--- src/components/CreateApp.vue
+++ src/components/CreateApp.vue
@@ -2,14 +2,42 @@
   <v-container class="form-container">
     <v-row>
       <v-col>
-        <v-text-field
-          label="Git-URL"
-          v-model="gitURL"
-          append-outer-icon="mdi-send"
-           @click:append-outer="downloadGit"
-          filled
-          clearable
-        ></v-text-field>
+        <v-alert
+          border="left"
+          color="pink darken-1"
+          v-if="error"
+          light
+          colored-border
+          elevation="2"
+          type="error"
+          >There was an error: {{ error }}</v-alert
+        >
+        <v-alert
+          border="left"
+          color="primary"
+          v-if="!success"
+          light
+          colored-border
+          elevation="2"
+          type="info"
+          >Erfolgreich!</v-alert
+        >
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-text-field label="Git-URL" v-model="gitURL" filled clearable>
+          <template v-slot:prepend-inner v-if="loading"
+            ><v-progress-circular
+              size="24"
+              color="info"
+              indeterminate
+            ></v-progress-circular>
+          </template>
+          <template v-slot:append-outer
+            ><v-icon @click="downloadGit">mdi-send</v-icon></template
+          >
+        </v-text-field>
       </v-col>
     </v-row>
     <v-row>
@@ -26,6 +54,9 @@ export default {
   data() {
     return {
       gitURL: "",
+      loading: false,
+      error: "",
+      success: false,
     };
   },
   components: {
@@ -34,7 +65,24 @@ export default {
   methods: {
     downloadGit() {
       console.log("Downloading: ", this.gitURL);
-      GitService.createProject(this.gitURL);
+      this.loading = true;
+      this.success = false;
+      this.error = "";
+      GitService.createProject(this.gitURL)
+        .then((e) => {
+          this.loading = false;
+          console.log("e: ", e);
+
+          if (e.error) {
+            this.error = e.error;
+          } else {
+            this.success = true;
+          }
+        })
+        .catch(() => {
+          this.loading = false;
+          // TODO: Show Error
+        });
     },
   },
 };
